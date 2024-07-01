26b911244c087a6f6e0dc7f36a7e83acbdf1581e
amabee <cyberkiller.ck@gmail.com>
2024-07-01 08:42:29 +0800

    update

--- app/page.js
+++ app/page.js
@@ -1211,7 +1211,7 @@ const Home = () => {
                 <Image src={"/assets/astro.png"} width={250} height={250} className='floatingAstroOnCard'></Image>
                 <div className="card-body">
                   <h5 className="card-title">Welcome to My Portfolio</h5>
-                  <p className="card-text mt-4 text-center desc">Hello and welcome to my portfolio! Im <b>John Paul Orencio</b>, a passionate software developer with a knack for creating elegant and efficient solutions. My journey in technology has been fueled by a deep curiosity and a drive to continuously learn and innovate.
+                  <p className="card-text mt-4 text-center desc">Hello and welcome to my portfolio! I&apos;m <b>John Paul Orencio</b>, a passionate software developer with a knack for creating elegant and efficient solutions. My journey in technology has been fueled by a deep curiosity and a drive to continuously learn and innovate.
                   </p>
                 </div>
               </div>
